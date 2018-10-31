55ae07026d6c7379610a3646300ed832d5dce112
Soojeong <soojeongshin2@gmail.com>
2018-11-01 05:23:51 +0900

    feat: Add required parameter

--- app/src/main/java/com/example/android/candypod/model/rss/Category.java
+++ app/src/main/java/com/example/android/candypod/model/rss/Category.java
@@ -6,7 +6,7 @@ import org.simpleframework.xml.Root;
 @Root(name = "category", strict = false)
 public class Category {
 
-    @Attribute(name = "text")
+    @Attribute(name = "text", required = false)
     private String mText;
 
     public Category() {
