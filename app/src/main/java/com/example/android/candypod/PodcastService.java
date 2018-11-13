f4651326c33ab509a9ce0999f723318b5f864f1c
Soojeong <soojeongshin2@gmail.com>
2018-11-13 23:06:28 +0900

    feat: Build a media browser service and client

---
+++ app/src/main/java/com/example/android/candypod/PodcastService.java
@@ -0,0 +1,309 @@
+/*
+ * Copyright 2018 Soojeong Shin
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.android.candypod;
+
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.media.MediaBrowserCompat;
+import android.support.v4.media.MediaBrowserServiceCompat;
+import android.support.v4.media.session.MediaSessionCompat;
+import android.support.v4.media.session.PlaybackStateCompat;
+import android.text.TextUtils;
+
+import com.example.android.candypod.ui.nowplaying.NowPlayingActivity;
+import com.google.android.exoplayer2.DefaultLoadControl;
+import com.google.android.exoplayer2.DefaultRenderersFactory;
+import com.google.android.exoplayer2.ExoPlaybackException;
+import com.google.android.exoplayer2.ExoPlayerFactory;
+import com.google.android.exoplayer2.LoadControl;
+import com.google.android.exoplayer2.Player;
+import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.Timeline;
+import com.google.android.exoplayer2.source.ExtractorMediaSource;
+import com.google.android.exoplayer2.source.MediaSource;
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
+import com.google.android.exoplayer2.trackselection.TrackSelector;
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
+import com.google.android.exoplayer2.util.Util;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import timber.log.Timber;
+
+/**
+ * Reference: @see "https://developer.android.com/guide/topics/media-apps/audio-app/building-a-mediabrowserservice"
+ * "https://github.com/googlesamples/android-UniversalMusicPlayer"
+ */
+public class PodcastService extends MediaBrowserServiceCompat implements Player.EventListener {
+
+    private static MediaSessionCompat mMediaSession;
+    private PlaybackStateCompat.Builder mStateBuilder;
+
+    private static final String CANDY_POD_ROOT_ID = "pod_root_id";
+    private static final String CANDY_POD_EMPTY_ROOT_ID = "empty_root_id";
+
+    /** Member variable for the ExoPlayer */
+    private SimpleExoPlayer mExoPlayer;
+
+    /** Tag for a MediaSessionCompat */
+    private static final String TAG = PodcastService.class.getSimpleName();
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        // Initialize the media session
+        initializeMediaSession();
+
+        // Initialize ExoPlayer
+        initializePlayer();
+    }
+
+    /**
+     * Initialize the media session.
+     */
+    private void initializeMediaSession() {
+        // Create a MediaSessionCompat
+        mMediaSession = new MediaSessionCompat(PodcastService.this, TAG);
+
+        // Enable callbacks from MediaButtons and TransportControls
+        mMediaSession.setFlags(
+                MediaSessionCompat.FLAG_HANDLES_MEDIA_BUTTONS |
+                        MediaSessionCompat.FLAG_HANDLES_TRANSPORT_CONTROLS);
+
+        // Set an initial PlaybackState with ACTION_PLAY, so media buttons can start the player
+        mStateBuilder = new PlaybackStateCompat.Builder()
+                .setActions(
+                        PlaybackStateCompat.ACTION_PLAY |
+                                PlaybackStateCompat.ACTION_PAUSE |
+                                PlaybackStateCompat.ACTION_REWIND |
+                                PlaybackStateCompat.ACTION_FAST_FORWARD |
+                                PlaybackStateCompat.ACTION_PLAY_PAUSE);
+        mMediaSession.setPlaybackState(mStateBuilder.build());
+
+        // MySessionCallback() has methods that handle callbacks from a media controller
+        mMediaSession.setCallback(new MySessionCallback());
+
+        // Set the session's token so that client activities can communicate with it.
+        setSessionToken(mMediaSession.getSessionToken());
+
+        mMediaSession.setSessionActivity(PendingIntent.getActivity(this,
+                11,
+                new Intent(this, NowPlayingActivity.class),
+                PendingIntent.FLAG_UPDATE_CURRENT));
+    }
+
+    /**
+     * Initialize ExoPlayer.
+     */
+    private void initializePlayer() {
+        if (mExoPlayer == null) {
+            // Create an instance of the ExoPlayer
+            DefaultRenderersFactory defaultRenderersFactory = new DefaultRenderersFactory(this);
+            TrackSelector trackSelector = new DefaultTrackSelector();
+            LoadControl loadControl = new DefaultLoadControl();
+            mExoPlayer = ExoPlayerFactory.newSimpleInstance(this, defaultRenderersFactory,
+                    trackSelector, loadControl);
+
+            // Set the Player.EventListener
+            mExoPlayer.addListener(this);
+
+            // Prepare the MediaSource
+            String testUrl = "http://feeds.soundcloud.com/stream/525464154-risepodcast-181105-rachel-hollis-rise-podcast-edmylett.m4a";
+//          Uri mediaUri = Uri.parse(mItem.getEnclosure().getUrl());
+            Uri mediaUri = Uri.parse(testUrl);
+            MediaSource mediaSource = buildMediaSource(mediaUri);
+            mExoPlayer.prepare(mediaSource);
+
+            mExoPlayer.setPlayWhenReady(true);
+        }
+    }
+
+    /**
+     * Release ExoPlayer.
+     */
+    private void releasePlayer() {
+        mExoPlayer.release();
+        mExoPlayer = null;
+    }
+
+    @Override
+    public void onTaskRemoved(Intent rootIntent) {
+        super.onTaskRemoved(rootIntent);
+        mExoPlayer.stop(true);
+        stopSelf();
+    }
+
+
+    @Override
+    public void onDestroy() {
+        mMediaSession.release();
+        releasePlayer();
+
+        super.onDestroy();
+    }
+
+    /**
+     * Create a MediaSource.
+     * @param mediaUri
+     */
+    private MediaSource buildMediaSource(Uri mediaUri) {
+        String userAgent = Util.getUserAgent(this, getString(R.string.app_name));
+        DefaultDataSourceFactory dataSourceFactory = new DefaultDataSourceFactory(
+                this, userAgent);
+        return new ExtractorMediaSource.Factory(dataSourceFactory).createMediaSource(mediaUri);
+    }
+
+    /**
+     * Returns the root node of the content hierarchy. This method controls access to the service.
+     */
+    @Nullable
+    @Override
+    public BrowserRoot onGetRoot(@NonNull String clientPackageName, int clientUid,
+                                 @Nullable Bundle rootHints) {
+        return new BrowserRoot(CANDY_POD_ROOT_ID, null);
+    }
+
+    /**
+     * This method provides the ability for a client to build and display a menu of the
+     * MediaBrowserService's content hierarchy.
+     */
+    @Override
+    public void onLoadChildren(@NonNull String parentMediaId,
+                               @NonNull Result<List<MediaBrowserCompat.MediaItem>> result) {
+
+        // Browsing not allowed
+        if (TextUtils.equals(CANDY_POD_EMPTY_ROOT_ID, parentMediaId)) {
+            result.sendResult(null);
+            return;
+        }
+
+        List<MediaBrowserCompat.MediaItem> mediaItems = new ArrayList<>();
+
+        // Check if this is the root menu:
+        if (CANDY_POD_ROOT_ID.equals(parentMediaId)) {
+            // Build the MediaItem objects for the top level,
+            // and put them in the mediaItems list...
+
+        } else {
+            // Examine the passed parentMediaId to see which submenu we're at,
+            // and put the children of that menu in the mediaItems list...
+        }
+        result.sendResult(mediaItems);
+    }
+
+    /**
+     * Media Session Callbacks, where all external clients control the player.
+     */
+    private class MySessionCallback extends MediaSessionCompat.Callback {
+
+        @Override
+        public void onPlay() {
+            // onPlay() callback should include code that calls startService().
+            startService(new Intent(getApplicationContext(), PodcastService.class));
+
+            // Set the session active
+            mMediaSession.setActive(true);
+
+            // Start the player
+            mExoPlayer.setPlayWhenReady(true);
+        }
+
+        @Override
+        public void onPause() {
+            mExoPlayer.setPlayWhenReady(false);
+
+            stopForeground(false);
+        }
+
+        @Override
+        public void onRewind() {
+            mExoPlayer.seekTo(Math.max(mExoPlayer.getCurrentPosition() - 10000, 0));
+            Timber.e("onRewind:");
+        }
+
+        @Override
+        public void onFastForward() {
+            long duration = mExoPlayer.getDuration();
+            mExoPlayer.seekTo(Math.min(mExoPlayer.getCurrentPosition() + 30000, duration));
+        }
+
+        @Override
+        public void onStop() {
+            // onStop() callback should call stopSelf().
+            stopSelf();
+
+            // Set the session inactive
+            mMediaSession.setActive(false);
+
+            // Stop the player
+            mExoPlayer.stop();
+
+            // Take the service out of the foreground
+            stopForeground(true);
+        }
+    }
+
+    // Player Event Listeners
+
+    @Override
+    public void onTimelineChanged(Timeline timeline, @Nullable Object manifest, int reason) {
+
+    }
+
+    @Override
+    public void onPlayerError(ExoPlaybackException error) {
+
+    }
+
+    @Override
+    public void onPlayerStateChanged(boolean playWhenReady, int playbackState) {
+        if (playbackState == Player.STATE_READY && playWhenReady) {
+            // When ExoPlayer is playing, update the PlaybackState
+            mStateBuilder.setState(PlaybackStateCompat.STATE_PLAYING,
+                    mExoPlayer.getCurrentPosition(), 1f);
+
+            Timber.d("onPlayerStateChanged: we are playing");
+        } else if (playbackState == Player.STATE_READY) {
+            // When ExoPlayer is paused, update the PlaybackState
+            mStateBuilder.setState(PlaybackStateCompat.STATE_PAUSED,
+                    mExoPlayer.getCurrentPosition(), 1f);
+
+            Timber.d("onPlayerStateChanged: we are paused");
+        }
+        mMediaSession.setPlaybackState(mStateBuilder.build());
+    }
+
+//    /**
+//     * Broadcast Receiver registered to receive the MEDIA_BUTTON intent coming from clients.
+//     */
+//    public static class MediaReceiver extends BroadcastReceiver {
+//
+//        public MediaReceiver() {
+//        }
+//
+//        @Override
+//        public void onReceive(Context context, Intent intent) {
+//            MediaButtonReceiver.handleIntent(mMediaSession, intent);
+//        }
+//    }
+}
--- app/src/main/java/com/example/android/candypod/ui/nowplaying/NowPlayingActivity.java
+++ app/src/main/java/com/example/android/candypod/ui/nowplaying/NowPlayingActivity.java
@@ -16,16 +16,268 @@
 
 package com.example.android.candypod.ui.nowplaying;
 
+import android.content.ComponentName;
+import android.content.Intent;
+import android.databinding.DataBindingUtil;
+import android.media.AudioManager;
 import android.os.Bundle;
+import android.os.RemoteException;
+import android.support.v4.media.MediaBrowserCompat;
+import android.support.v4.media.MediaMetadataCompat;
+import android.support.v4.media.session.MediaControllerCompat;
+import android.support.v4.media.session.MediaSessionCompat;
+import android.support.v4.media.session.PlaybackStateCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.SeekBar;
 
+import com.bumptech.glide.Glide;
+import com.example.android.candypod.PodcastService;
 import com.example.android.candypod.R;
+import com.example.android.candypod.databinding.ActivityNowPlayingBinding;
+import com.example.android.candypod.model.rss.Item;
+import com.example.android.candypod.model.rss.ItemImage;
 
+import timber.log.Timber;
+
+import static com.example.android.candypod.utilities.Constants.EXTRA_ITEM;
+import static com.example.android.candypod.utilities.Constants.EXTRA_PODCAST_IMAGE;
+
+/**
+ * Reference: @see "https://developer.android.com/guide/topics/media-apps/audio-app/building-a-mediabrowser-client"
+ */
 public class NowPlayingActivity extends AppCompatActivity {
+
+    /** The podcast episode */
+    private Item mItem;
+
+    /** The podcast image URL used when there is no the current episode's image */
+    private String mPodcastImage;
+
+    /** The MediaBrowser connects to a MediaBrowserService, and upon connecting it creates the
+     * MediaController for the UI*/
+    private MediaBrowserCompat mMediaBrowser;
+
+    /** This field is used for data binding */
+    private ActivityNowPlayingBinding mNowPlayingBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_nowplaying);
+        mNowPlayingBinding = DataBindingUtil.setContentView(
+                this, R.layout.activity_now_playing);
+
+        setupUI();
+
+        Timber.d("enclosure url: " + mItem.getEnclosure().getUrl());
+
+        // Create MediaBrowserCompat
+        createMediaBrowserCompat();
     }
+
+    /**
+     * Setup UI
+     */
+    private void setupUI() {
+        // Get the podcast episode and the podcast image URL
+        getItemAndImage();
+
+        String title = mItem.getTitle();
+        if (title != null) {
+            mNowPlayingBinding.tvNowTitle.setText(title);
+        }
+
+        // Not all episode has its image. If it exists, use the episode image. Otherwise,
+        // use the podcast image.
+        String itemImageUrl = mPodcastImage;
+        ItemImage itemImage = mItem.getItemImage();
+        if (itemImage != null) {
+            itemImageUrl = itemImage.getItemImageHref();
+        }
+        // Use Glide library to upload the image
+        Glide.with(this)
+                .load(itemImageUrl)
+                .into(mNowPlayingBinding.ivNowEpisode);
+    }
+
+    /**
+     * Constructs a MediaBrowserCompat.
+     */
+    private void createMediaBrowserCompat() {
+        mMediaBrowser = new MediaBrowserCompat(this,
+                new ComponentName(this, PodcastService.class),
+                mConnectionCallbacks,
+                null);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        // Connect to the MediaBrowserService
+        mMediaBrowser.connect();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        // Set the audio stream so the app responds to the volume control on the device
+        setVolumeControlStream(AudioManager.STREAM_MUSIC);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        // Disconnect the MediaBrowser and unregister the MediaController.Callback when the
+        // activity stops
+        if (MediaControllerCompat.getMediaController(this) != null) {
+            MediaControllerCompat.getMediaController(this).unregisterCallback(controllerCallback);
+        }
+        mMediaBrowser.disconnect();
+    }
+
+    /**
+     * Get the podcast episode and the podcast image URL from the DetailActivity via Intent.
+     */
+    private void getItemAndImage() {
+        Intent intent = getIntent();
+        if (intent != null) {
+            if (intent.hasExtra(EXTRA_ITEM)) {
+                Bundle b = intent.getBundleExtra(EXTRA_ITEM);
+                mItem = b.getParcelable(EXTRA_ITEM);
+            }
+            if (intent.hasExtra(EXTRA_PODCAST_IMAGE)) {
+                mPodcastImage = intent.getStringExtra(EXTRA_PODCAST_IMAGE);
+            }
+        }
+    }
+
+    private final MediaBrowserCompat.ConnectionCallback mConnectionCallbacks =
+            new MediaBrowserCompat.ConnectionCallback() {
+                @Override
+                public void onConnected() {
+
+                    // Get the token for the MediaSession
+                    MediaSessionCompat.Token token = mMediaBrowser.getSessionToken();
+
+                    // Create a MediaControllerCompat
+                    MediaControllerCompat mediaController = null;
+                    try {
+                        mediaController = new MediaControllerCompat(NowPlayingActivity.this, token);
+                    } catch (RemoteException e) {
+                        Timber.e("Error creating media controller");
+                    }
+
+                    // Save the controller
+                    MediaControllerCompat.setMediaController(NowPlayingActivity.this,
+                            mediaController);
+
+                    // Finish building the UI
+                    buildTransportControls();
+                }
+
+                @Override
+                public void onConnectionSuspended() {
+                    super.onConnectionSuspended();
+                    // The Service has crashed. Disable transport controls until it automatically
+                    // reconnects.
+                }
+
+                @Override
+                public void onConnectionFailed() {
+                    super.onConnectionFailed();
+                    // The Service has refused our connection
+                }
+            };
+
+    void buildTransportControls() {
+        // Attach a listener to the play/pause button
+        mNowPlayingBinding.ibPlayPause.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int pbState = MediaControllerCompat.getMediaController(NowPlayingActivity.this)
+                        .getPlaybackState().getState();
+
+                if (pbState == PlaybackStateCompat.STATE_PLAYING) {
+                    MediaControllerCompat.getMediaController(NowPlayingActivity.this)
+                            .getTransportControls().pause();
+
+                } else {
+                    MediaControllerCompat.getMediaController(NowPlayingActivity.this)
+                            .getTransportControls().play();
+
+                }
+            }
+        });
+
+        // Attach a listener to the fast forward button
+        mNowPlayingBinding.ibFastforward.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MediaControllerCompat.getMediaController(NowPlayingActivity.this)
+                        .getTransportControls().fastForward();
+            }
+        });
+
+        // Attach a listener to the rewind button
+        mNowPlayingBinding.ibRewind.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MediaControllerCompat.getMediaController(NowPlayingActivity.this)
+                        .getTransportControls().rewind();
+            }
+        });
+
+        mNowPlayingBinding.seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        MediaControllerCompat mediaController = MediaControllerCompat.getMediaController(NowPlayingActivity.this);
+
+        // Display the initial state
+        MediaMetadataCompat metadata = mediaController.getMetadata();
+        PlaybackStateCompat pbState = mediaController.getPlaybackState();
+
+        if (pbState.getState() == PlaybackStateCompat.STATE_PLAYING) {
+            mNowPlayingBinding.ibPlayPause.setImageResource(R.drawable.exo_controls_pause);
+        } else {
+            mNowPlayingBinding.ibPlayPause.setImageResource(R.drawable.exo_controls_play);
+        }
+
+        // Register a Callback to stay in sync
+        mediaController.registerCallback(controllerCallback);
+    }
+
+    /**
+     * Callback for receiving updates from the session.
+     */
+    MediaControllerCompat.Callback controllerCallback = new MediaControllerCompat.Callback() {
+        @Override
+        public void onMetadataChanged(MediaMetadataCompat metadata) {
+            super.onMetadataChanged(metadata);
+        }
+
+        @Override
+        public void onPlaybackStateChanged(PlaybackStateCompat state) {
+            super.onPlaybackStateChanged(state);
+            if (state.getState() == PlaybackStateCompat.STATE_PLAYING) {
+                mNowPlayingBinding.ibPlayPause.setImageResource(R.drawable.exo_controls_pause);
+            } else {
+                mNowPlayingBinding.ibPlayPause.setImageResource(R.drawable.exo_controls_play);
+            }
+        }
+    };
 }
