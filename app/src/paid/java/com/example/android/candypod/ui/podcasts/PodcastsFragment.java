8bf427c574f5b2f07627115c01d8b0705b69350c
Soojeong <soojeongshin2@gmail.com>
2018-12-09 19:58:04 +0900

    feat: Hide FAB when scrolling

--- app/src/paid/java/com/example/android/candypod/ui/podcasts/PodcastsFragment.java
+++ app/src/paid/java/com/example/android/candypod/ui/podcasts/PodcastsFragment.java
@@ -23,9 +23,11 @@ import android.content.Context;
 import android.content.Intent;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityOptionsCompat;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -81,6 +83,9 @@ public class PodcastsFragment extends Fragment
 
         // Create a GridAutofitLayoutManager and PodcastsAdapter, and set them to the RecyclerView
         initAdapter();
+
+        // Hide FAB when scrolling
+        hideShowFab();
 
         return rootView;
     }
@@ -204,5 +209,33 @@ public class PodcastsFragment extends Fragment
         // Set text programmatically in order to make text invisible when the user changes the menu
         // items in the navigation drawer
         mPodcastsBinding.tvEmptyPodcasts.setText(getString(R.string.empty_podcasts));
+    }
+
+    /**
+     * Hides FAB when scrolling.
+     * Reference: @see "https://stackoverflow.com/questions/33208613/hide-floatingactionbutton-on-scroll-of-recyclerview"
+     */
+    private void hideShowFab() {
+        // Add a listener to be notified of any changes in scroll state
+        mPodcastsBinding.rvPodcasts.addOnScrollListener(new RecyclerView.OnScrollListener() {
+
+            @Override
+            public void onScrolled(@NonNull RecyclerView recyclerView, int dx, int dy) {
+                super.onScrolled(recyclerView, dx, dy);
+                // Hide FAB when the RecyclerView is scrolling
+                if (dy > 0 || dy < 0 && mPodcastsBinding.fab.isShown()) {
+                    mPodcastsBinding.fab.hide();
+                }
+            }
+
+            @Override
+            public void onScrollStateChanged(@NonNull RecyclerView recyclerView, int newState) {
+                super.onScrollStateChanged(recyclerView, newState);
+                // Show FAB when the RecyclerView is not currently scrolling
+                if (newState == RecyclerView.SCROLL_STATE_IDLE) {
+                    mPodcastsBinding.fab.show();
+                }
+            }
+        });
     }
 }
